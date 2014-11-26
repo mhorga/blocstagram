a42f36d82ab7edf018cfa95bb3471ed5346e9e26
Marius Horga <mhorga@gmail.com>
2014-11-26 02:09:03 +0200

    Table View controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Marius Horga on 11/26/14.
+//  Copyright (c) 2014 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
