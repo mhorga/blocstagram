ea356fc70b8021646dfccfa26dc454403096d959
Marius Horga <mhorga@gmail.com>
2014-12-16 18:13:41 +0200

    Photo Filters and Sending to Instagram

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Marius Horga on 12/16/14.
+//  Copyright (c) 2014 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
